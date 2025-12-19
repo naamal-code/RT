af66647dc03357d266b46642f18c16d3b747830c
naamal-code <vp96nx5g6m@privaterelay.appleid.com>
2025-12-19 13:01:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* sw.js - offline cache for this PWA */
 
-const CACHE_NAME = "data-calc-v4"; // 更新したら v3, v4... に上げる
+const CACHE_NAME = "data-calc-v5"; // 更新したら v3, v4... に上げる
 
 // キャッシュするのは「本当に必要な最小限」に限定
 const PRECACHE_URLS = [
@@ -104,4 +104,5 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
 
